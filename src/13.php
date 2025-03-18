55437f8cc777bcaab5ac1c0082aa877a2d57cf56
akermangardener89 <akermangardener89@gmail.com>
2025-03-18 06:25:47 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,12 @@
+<?php
+// get a random number between 1 and 10
+$randomNumber = rand(1, 10);
+
+// multiply the random number by 2
+$doubledNumber = $randomNumber * 2;
+
+// subtract 3 from the doubled number
+$result = $doubledNumber - 3;
+
+echo "The result is: $result";
+?>
