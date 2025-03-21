01194cb94b45f535fa6c55849c20f5cd8d7a9363
akermangardener89 <akermangardener89@gmail.com>
2025-03-21 07:11:54 +0000

    Add 16.php

---
+++ src/16.php
@@ -0,0 +1,15 @@
+<?php
+// Example PHP code to demonstrate basic operations and variables
+
+// Declare variables
+$name = "John";
+$age = 28;
+
+// Print values of variables
+echo "The name is: $name\n";
+echo "My age is: $age\n";
+
+// Calculate a simple math problem
+$result = $age + 10;
+echo "Result of adding 10 to my age: $result\n";
+?>
