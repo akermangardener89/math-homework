4d09966c7c308df9a22b3eb8df317c100754e040
akermangardener89 <akermangardener89@gmail.com>
2025-03-23 21:17:16 +0000

    Add 18.php

---
+++ src/18.php
@@ -0,0 +1,10 @@
+<?php 
+    $username = "student123";
+    $password = "pass123";
+    
+    if ($username == "student123" && $password == "pass123") {
+        echo "Login successful!";
+    } else {
+        echo "Login failed.";
+    }
+?>
