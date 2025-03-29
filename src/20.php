3444943b09587f1a2d9244b419818d304bff7824
akermangardener89 <akermangardener89@gmail.com>
2025-03-29 19:46:46 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,15 @@
+<?php
+// Define an array of data points
+$data_points = [
+    "point1" => 5,
+    "point2" => -3,
+    "point3" => 8,
+    "point4" => -2.5,
+    "point5" => 7,
+];
+
+// Loop through the array and display each point
+foreach ($data_points as $key => $value) {
+    echo $key . ": " . $value . "<br>";
+}
+?>
