649365db2b57ceddb87ba00e3927a4de02b9827f
akermangardener89 <akermangardener89@gmail.com>
2025-04-03 05:50:35 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,8 @@
+<?php
+// Define variables and constants
+$var1 = "Example value 1";
+$var2 = "Example value 2";
+
+// This is just an example of how you can use these variables
+echo "The values are: $var1 and $var2. ";
+?>
