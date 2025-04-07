de130ad3f1f50d53b7b2f751e66462418a62b1e7
akermangardener89 <akermangardener89@gmail.com>
2025-04-07 05:17:11 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,9 @@
+<?php
+// This is a simple PHP code to demonstrate basic functions.
+// You can modify and use this code as needed.
+function add_two_numbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+echo "The sum of 5 and 3 is: " . add_two_numbers(5, 3);
+?>
